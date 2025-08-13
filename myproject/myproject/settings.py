bde118f1fe41bda44672a4ccafd6bce7f617eb3e
Absolute075 <absolute07589@gmail.com>
2025-08-13 19:50:44 +0500

    HOST

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,8 +11,7 @@ SECRET_KEY = 'django-insecure-zmh04bp-a+fxk%jnd-kg@5kwn=tc)8wwc%pk25cqxjd@sc5_o8
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.100.145', '127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = ['192.168.100.145', '127.0.0.1', 'localhost', 'study-app-a53x.onrender.com']
 
 
 
