025cd3ab5dc4d9e0507d56f8c49c996aa555bbc5
Absolute075 <absolute07589@gmail.com>
2025-09-03 17:29:55 +0500

    minus django XD

--- myproject/accounts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import RegisterView, blocked_view
-
-urlpatterns = [
-    path('api/register/', RegisterView.as_view(), name='register'),
-    path('blocked/', blocked_view, name='blocked'),
-]
